28dbf960943269f68dd5165e15da25e0d9c50dce
Sergey Skorokhod <40693196+IllusionOfControl@users.noreply.github.com>
2023-04-20 18:23:57 +0300

    removed credetials

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config():
-    BOT_TOKEN = '1439631404:AAFvCccYJ4hzKZj6sp8wHYOgrlzv9CGO5UA'
-    BOT_WEBHOOK_URL = 'https://professor-bsuir-bot.herokuapp.com/'
+    BOT_TOKEN = 'bot:token'
+    BOT_WEBHOOK_URL = 'bot:webhook:url'
 
-    BSUIR_USERNAME = '08107123'
-    BSUIR_PASSWORD =  'HPqC5MAEJSdZF6L'
+    BSUIR_USERNAME = 'bsuir:username'
+    BSUIR_PASSWORD =  'bsuir:password'
